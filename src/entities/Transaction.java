826e297a644749dbb9b3d2f72ab67969a4226ab2
Pavel <35321137+paveldanek@users.noreply.github.com>
2023-02-07 23:08:32 -0600

    Transaction fields update.

--- src/entities/Transaction.java
+++ src/entities/Transaction.java
@@ -8,19 +8,19 @@ public class Transaction {
 			OTHER = 50;
 //	many more to be added	
 
-	private Calendar transDate;
 	private Calendar postedDate;
 	private String refNumber;
 	private String description;
+	private String memo;
 	private double amount;
 	private int category;
 
-	public Transaction(Calendar transDate, Calendar postedDate, String refNumber, String description, double amount,
+	public Transaction(Calendar postedDate, String refNumber, String description, String memo, double amount,
 			int category) {
-		this.transDate = transDate;
 		this.postedDate = postedDate;
 		this.refNumber = refNumber;
 		this.description = description;
+		this.memo = memo;
 		this.amount = amount;
 		this.category = category;
 	}
@@ -91,14 +91,6 @@ public class Transaction {
 
 // --------------- getters and setters ------------------------------------	
 
-	public Calendar getTransDate() {
-		return transDate;
-	}
-
-	public void setTransDate(Calendar transDate) {
-		this.transDate = transDate;
-	}
-
 	public Calendar getPostedDate() {
 		return postedDate;
 	}
@@ -121,6 +113,14 @@ public class Transaction {
 
 	public void setDescription(String description) {
 		this.description = description;
+	}
+
+	public String getMemo() {
+		return memo;
+	}
+
+	public void setMemo(String memo) {
+		this.memo = memo;
 	}
 
 	public double getAmount() {
@@ -150,10 +150,6 @@ public class Transaction {
 //  if POSITIVE number is returned, the calling Transaction
 //	is "GREATER THAN" the Transaction in parameters,
 
-	public int compareTransDates(Transaction transaction) {
-		return this.transDate.compareTo(transaction.getTransDate());
-	}
-
 	public int comparePostedDates(Transaction transaction) {
 		return this.postedDate.compareTo(transaction.getPostedDate());
 	}
@@ -164,6 +160,10 @@ public class Transaction {
 
 	public int compareDescriptions(Transaction transaction) {
 		return this.description.compareTo(transaction.getDescription());
+	}
+
+	public int compareMemos(Transaction transaction) {
+		return this.memo.compareTo(transaction.getMemo());
 	}
 
 	public double compareAmounts(Transaction transaction) {
@@ -177,16 +177,16 @@ public class Transaction {
 // ------------------- other methods ---------------------------------------	
 
 	public boolean isBetweenDates(Calendar firstDate, Calendar secondDate) {
-		return (!transDate.before(firstDate) && !transDate.after(secondDate));
+		return (!postedDate.before(firstDate) && !postedDate.after(secondDate));
 	}
 
 	@Override
 	public String toString() {
 		String output = "THIS TRANSACTION:\n------------------------------------\n";
-		output += "Transaction Date: " + returnMMslashDDFromCalendar(this.transDate) + "\n";
 		output += "Posted Date:      " + returnMMslashDDFromCalendar(this.postedDate) + "\n";
 		output += "Reference Number: " + this.refNumber + "\n";
 		output += "Description:      " + this.description + "\n";
+		output += "Memo:             " + this.memo + "\n";
 		output += "Amount:           " + String.format("$ %.2f", amount) + "\n";
 		output += "Category:         " + this.category + "\n------------------------------------\n";
 		return output;
@@ -195,17 +195,17 @@ public class Transaction {
 //	public static void main(String[] args) {
 //		System.out.println(returnOFXFromCalendar(returnCalendarFromOFX("20220415201405")));
 //		System.out.println("\n\n");
-//		Transaction t1 = new Transaction(returnCalendarFromMMslashDD("02/21", "2022"),
-//				returnCalendarFromMMslashDD("02/22", "2022"), "S254WD98422000A1", "Cub Foods #1224", 126.74, ESSENTIALS);
+//		Transaction t1 = new Transaction(returnCalendarFromMMslashDD("02/22", "2022"), 
+//				"S254WD98422000A1", "Cub Foods #1224", "Quick groceries...", 126.74, ESSENTIALS);
 //		Transaction t2 = new Transaction(returnCalendarFromMMslashDD("02/21", "2022"),
-//				returnCalendarFromMMslashDD("02/21", "2022"), "R254WD98422000A1", "Rainbow Foods", 122, ESSENTIALS);
+//				"R254WD98422000A1", "Rainbow Foods", "Birthday present purchase", 122, ESSENTIALS);
 //		System.out.println(t1);
 //		System.out.println(t2 + "\n\n");
 //		System.out.println("COMPARISONS:\n------------------");
-//		System.out.println("Trans Dates:    " + t1.compareTransDates(t2) + "\n");
 //		System.out.println("Posted Dates:   " + t1.comparePostedDates(t2) + "\n");
 //		System.out.println("Ref. Numbers:   " + t1.compareRefNumbers(t2) + "\n");
 //		System.out.println("Descriptions:   " + t1.compareDescriptions(t2) + "\n");
+//		System.out.println("Memos:          " + t1.compareMemos(t2) + "\n");	
 //		System.out.println("Amounts:        " + t1.compareAmounts(t2) + "\n");
 //		System.out.println("Categories:     " + t1.compareCategories(t2) + "\n------------------");
 //	}
